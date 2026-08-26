a68015b506bf1fd8662a9aa86e2e0611fc40070f
otabek-4662 <277716469+otabek-4662@users.noreply.github.com>
2026-08-26 15:41:01 +0500

    fix: Swagger Authorize button - add bearer JWT security scheme

--- src/main/java/com/taskcenter/config/SwaggerConfig.java
+++ src/main/java/com/taskcenter/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.taskcenter.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,9 @@ public class SwaggerConfig {
             renderUrl = "https://task-center-backend.onrender.com";
         }
         return new OpenAPI()
+            .components(new Components().addSecuritySchemes("bearerAuth",
+                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
             .addServersItem(new Server().url("http://localhost:8080").description("Local - localhost:8080"))
             .addServersItem(new Server().url(renderUrl).description("Render - production"))
             .addServersItem(new Server().url("https://schematic-crewmate-repaint.ngrok-free.app").description("Ngrok - old tunnel"));
