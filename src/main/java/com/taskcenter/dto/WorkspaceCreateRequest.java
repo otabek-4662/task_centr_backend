4bac196dc7a4c15f40d2558f4d091b728a2b758f
otabek-4662 <277716469+otabek-4662@users.noreply.github.com>
2026-09-15 21:11:18 +0500

    bgColor validatsiya kengaytirildi (gradient qabul qiladi, max 255)

--- src/main/java/com/taskcenter/dto/WorkspaceCreateRequest.java
+++ src/main/java/com/taskcenter/dto/WorkspaceCreateRequest.java
@@ -1,7 +1,6 @@
 package com.taskcenter.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,8 +10,7 @@ public class WorkspaceCreateRequest {
     @Size(max = 255, message = "title 255 belgidan oshmasligi kerak")
     private String title;
 
-    @Size(max = 30, message = "bgColor 30 belgidan oshmasligi kerak")
-    @Pattern(regexp = "^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$", message = "bgColor hex rang bo'lishi kerak")
+    @Size(max = 255, message = "bgColor 255 belgidan oshmasligi kerak")
     private String bgColor;
 
     @Size(max = 5000, message = "description 5000 belgidan oshmasligi kerak")
